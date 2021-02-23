d415cfd92d5be56b8b383823c53bfeccf67f5e4f
“zed” <zed@kolk.me>
2021-02-22 23:32:20 -0600

    finished

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-// Write your solution here!
+var drivers = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendDriver(name) {
+    // drivers = [...drivers, name];
+    drivers.push(name)
+    return drivers
+}
+
+function destructivelyPrependDriver(name) {
+    drivers.unshift(name)
+    return drivers
+}
+
+function destructivelyRemoveLastDriver() {
+    drivers.pop()
+    return drivers
+}
+
+function destructivelyRemoveFirstDriver() {
+    drivers.shift()
+    return drivers
+}
+
+function appendDriver(name) {
+    // return drivers.concat(name)
+    return [...drivers, name]
+}
+
+function prependDriver(name) {
+    return [name, ...drivers]
+}
+
+function removeLastDriver() {
+    return drivers.slice(0, drivers.length-1)
+}
+
+function removeFirstDriver() {
+    return drivers.slice(1, drivers.length)
+}
